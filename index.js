ddff8500d3adcb9cb6e1cccfcab1ebd2d015ffc5
Mikelanne <mikelanne.wood@gmail.com>
2021-03-14 20:01:47 -0400

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let main = document.querySelector("main#main")
+
+main.remove()
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "MIKELANNE is the champion";
+document.body.appendChild(newHeader)
+
